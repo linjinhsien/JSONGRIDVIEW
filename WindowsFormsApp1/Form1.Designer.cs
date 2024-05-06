4fcfcd133227e2c4716f02f8ef925bc8193b11c5
iven8 <iven8@MSI>
2024-05-06 22:13:53 +0800

    加入專案檔案。

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.Size = new System.Drawing.Size(1166, 469);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1166, 469);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.AspNetCore.JsonPatch.Operations;
+using Newtonsoft.Json;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private const string JsonUrl = "https://openapi.twse.com.tw/v1/exchangeReport/BWIBBU_ALL";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            string currentDirectory = Directory.GetCurrentDirectory();
+            string relativePath = @"json\BWIBBU_ALL.json";
+            string jsonFilePath = Path.Combine(currentDirectory, relativePath);
+
+            await DownloadJsonFileAsync(jsonFilePath);
+            LoadJsonToDataGridView(jsonFilePath);
+            Form1_Resize(sender, e);
+        }
+
+        private async Task DownloadJsonFileAsync(string filePath)
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    HttpResponseMessage response = await client.GetAsync(JsonUrl);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string json = await response.Content.ReadAsStringAsync();
+                        File.WriteAllText(filePath, json);
+                        Console.WriteLine($"JSON 檔案下載成功：{filePath}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"下載 JSON 檔案失敗：{response.StatusCode}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"下載 JSON 檔案時發生錯誤：{ex.Message}");
+            }
+        }
+
+        private void LoadJsonToDataGridView(string jsonFilePath)
+        {
+            // Read the JSON data from the file
+            string json = File.ReadAllText(jsonFilePath);
+
+            // Deserialize the JSON string into a List<Stock> object
+            List<Stock> stocks = JsonConvert.DeserializeObject<List<Stock>>(json);
+
+            // Convert the List<Stock> to a DataTable
+            DataTable dataTable = new DataTable();
+            dataTable.Columns.Add("Code");
+            dataTable.Columns.Add("Name");
+            dataTable.Columns.Add("PEratio");
+            dataTable.Columns.Add("DividendYield");
+            dataTable.Columns.Add("PBratio");
+
+
+
+
+            foreach (var stock in stocks)
+            {
+                dataTable.Rows.Add(stock.Code, stock.Name, stock.PEratio, stock.DividendYield, stock.PBratio);
+            }
+
+            dataTable.Columns["Code"].ColumnName = "股票代碼";
+            dataTable.Columns["Name"].ColumnName = "股票名稱";
+            dataTable.Columns["PEratio"].ColumnName = "本益比";
+            dataTable.Columns["DividendYield"].ColumnName = "殖利率";
+            dataTable.Columns["PBratio"].ColumnName = "股價淨值比";
+
+            // Bind the DataTable to the DataGridView
+            dataGridView1.DataSource = dataTable;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            int Columncount = dataGridView1.ColumnCount;
+            int Width = dataGridView1.Width - 70;
+            foreach (DataGridViewColumn column in dataGridView1.Columns)
+            {
+                column.Width = Width / Columncount;
+            }
+        }
+    }
+
+    public class Stock
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string PEratio { get; set; }
+        public string DividendYield { get; set; }
+        public string PBratio { get; set; }
+    }
+}
